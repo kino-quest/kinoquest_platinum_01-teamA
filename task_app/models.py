14f43b108d5d7030f9ea11262ab13cb89b2ba675
Tsuzumi Nakamura <smallkabutomsi@gmail.com>
2025-04-16 16:10:10 +0900

    no message

--- task_app/models.py
+++ task_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 # カスタマーテーブル
-class Customer(models.Model):
+class CustomUser(models.Model):
   username = models.CharField(max_length=30)
   mail = models.EmailField(max_length=255)
   password = models.CharField(max_length=255)
