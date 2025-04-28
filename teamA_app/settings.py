29ef94ce89e0f9c9bb405e76e06508a4ae0e1ccc
yosuke <bigstart122091@gmail.com>
2025-04-28 16:18:17 +0900

    アプリケーションの追加をしました。

--- teamA_app/settings.py
+++ teamA_app/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_app'
+    'task_app',
+    'accounts_app',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts_app.CustomUser'
