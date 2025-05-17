ffa48e02f30a5971ab1ca4d17e29043c08bb437b
Tsuzumi Nakamura <smallkabutomsi@gmail.com>
2025-05-17 16:50:27 +0900

    feature-38

--- task_app/urls.py
+++ task_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('privacy/', views.privacy, name="privacy"),
+    path('rules/', views.rules, name="rules"),
 ]
--- task_app/views.py
+++ task_app/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     print("home")
-    return render(request, 'task_app/base.html')
+    return render(request, 'task_app/home.html')
+
+def privacy(request):
+    return render(request, 'task_app/privacy.html')
+
+def rules(request):
+    return render(request, 'task_app/rules.html')
