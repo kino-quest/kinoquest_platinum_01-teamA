d0d08e8a333f612277238226eba03a1670a91be8
yosuke <bigstart122091@gmail.com>
2025-05-20 10:59:36 +0900

    ユーザー情報設定を追加しました

--- accounts_app/forms.py
+++ accounts_app/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, UserCreationForm
 
+#新規登録
 class SignupForm(UserCreationForm):
     class Meta:
         model = get_user_model()
@@ -28,6 +29,7 @@ class SignupForm(UserCreationForm):
         self.fields['password1'].label = 'パスワード'
         self.fields['password2'].label = '確認用パスワード'
 
+#ログイン
 class LoginForm(AuthenticationForm):
 
     def __init__(self, *args, **kwargs):
@@ -47,9 +49,51 @@ class LoginForm(AuthenticationForm):
         self.fields['username'].label = 'ユーザー名'
         self.fields['password'].label = 'パスワード'
     
+    #もし「admin」という文字を含むのを禁止したい場合に使用するメソッド
     def clean_username(self):
         username = self.cleaned_data.get('username')
         
         if 'admin' in username:
             raise forms.ValidationError('ユーザー名に "admin" を含めることはできません。')
         return username
+
+#編集 ユーザー名、メールアドレスのカスタマイズ
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email']
+    
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ''
+    
+        if user:
+            self.fields['username'].widget.attrs['placeholder'] = f"{user.username}"
+            self.fields['email'].widget.attrs['placeholder'] = f"{user.email}"
+
+        for field_name in ['username', 'email']:
+            self.fields[field_name].widget.attrs['class'] = 'form-control w-40'
+
+        self.fields['username'].label = 'ユーザー名'
+        self.fields['email'].label = 'メールアドレス'
+
+#編集 PasswordChangeFormをカスタマイズ(オーバーライド)
+class CustomPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ''
+
+        field_settings = {
+            'old_password': '現在のパスワードを入力してください',
+            'new_password1': '新しいパスワードを入力してください',
+            'new_password2': '確認用の新しいパスワードを入力してください',
+        }
+
+        for field_name, placeholder in field_settings.items():
+            field = self.fields[field_name]
+            field.widget.attrs['class'] = 'form-control w-40'
+            field.widget.attrs['placeholder'] = placeholder
+
+        self.fields['old_password'].label = '現在のパスワード'
+        self.fields['new_password2'].label = '新しいパスワード確認'
--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,51 +1,75 @@
-from django.contrib.auth import login, logout
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
-from .forms import LoginForm, SignupForm
+from .forms import LoginForm, SignupForm, CustomPasswordChangeForm, UserUpdateForm
 
 
 # 新規登録
 def signup_view(request):
-    print("signup")
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('home')    
     else:
         form = SignupForm()
-    params = {
-        'form': form,
-    }
-    return render(request, 'accounts_app/signup.html', params)
+    return render(request, 'accounts_app/signup.html', {'form': form})
 
 # ログイン
 def login_view(request):
-    print("login")
     if request.method == 'POST':
         form = LoginForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
-
             if user is not None:
                 login(request, user)
                 return redirect('home')
-            else:
-                form.add_error(None, 'パスワードが正しくありません。')
-                params = {
-                    'form': form,
-                }
-            return render(request, 'accounts_app/login.html', params)
-        else:
-            print("isn't valid")
-            print(form.errors)
-            params = {
-                'form': form,
-            }
-            return render(request, 'accounts_app/login.html', params)
+            #ログイン失敗
+            form.add_error(None, 'ログインに失敗しました')
+        #バリデーション失敗 or ユーザーがNoneのどちらもここで処理
+        return render(request, 'accounts_app/login.html', {'form': form})
     else:
         form = LoginForm()
+    return render(request, 'accounts_app/login.html', {'form': form})
+
+#ログアウト
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+#ユーザー情報設定
+@login_required
+def user_settings(request):
+    #すべての項目を埋めて「更新」ボタンが押された時
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        password_form = CustomPasswordChangeForm(user=request.user, data=request.POST)
+
+        user_valid = user_form.is_valid()
+        password_valid = password_form.is_valid()
+
+        if user_valid:
+            user_form.save()
+        
+        if password_valid:
+            user = password_form.save()
+            #パスワード変更後にログアウトされるのを防ぐ
+            update_session_auth_hash(request, user)
+        if not user_valid and not password_valid:
+            messages.error(request, '入力内容に誤りがあります。確認してください')
+        # どちらも成功していたら「/user_settings」へリダイレクト
+        if user_valid and password_valid:
+            messages.success(request, 'ユーザー情報を更新しました')
+            return redirect('user_settings')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        password_form = CustomPasswordChangeForm(user=request.user)
+    
     params = {
-        'form': form,
+        'user_form': user_form,
+        'password_form': password_form,
     }
-    return render(request, 'accounts_app/login.html', params)
+    return render(request, 'accounts_app/user_info.html', params)
